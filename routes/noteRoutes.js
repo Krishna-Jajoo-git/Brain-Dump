ee9edce89bcfd2638cb338c76ad955fc7848a494
Krishna <roshniso82@gmail.com>
2026-03-23 00:14:59 +0530

    Full CRUD logic completed with proper error handling

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -10,7 +10,7 @@ router.post('/',async(req,res)=>{
         const savedNote = await newNote.save();
         res.status(201).json(savedNote)
     }catch(error){
-        res.status(400).json({Error:error.massage})
+        res.status(400).json({Error:error.message})
     }
 })
 
@@ -19,7 +19,32 @@ router.get('/',async (req,res)=>{
         const allNotes= await Note.find();
         res.status(200).json(allNotes);
     }catch(error){
-        res.status(500).json({Error : error.massage})
+        res.status(500).json({Error : error.message})
+    }
+});
+
+router.delete('/:id',async (req,res)=>{
+    try{
+        const deletedNote=await Note.findByIdAndDelete(req.params.id)
+        if(!deletedNote){
+            return res.status(404).json({message:"Note not found"})
+        }
+        res.status(200).json({message:"Note deleted Successfully",deletedNote})
+    }catch(error){
+        return res.status(500).json({Error: error.message})
+    }
+})
+
+router.put('/:id',async (req,res)=>{
+    try{
+        const {title,description}=req.body;
+        const updatedNote=await Note.findByIdAndUpdate(req.params.id,{title,description},{new: true})
+        if(!updatedNote){
+            return res.status(404).json({message:"Note not found"})
+        }
+        res.status(200).json({message:"Note Updated Successfully",updatedNote})
+    }catch(error){
+        res.status(500).json({Error: error.message})
     }
 })
 
